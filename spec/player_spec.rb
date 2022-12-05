9f71075e12fcf17bb370ec5bb931f1c2e1d5dcf1
Austin Miller <austinmillerwebdev@gmail.com>
2022-12-05 01:37:29 -0800

    Add player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,2 +1,15 @@
 # frozen_string_literal: true
 
+require './lib/player'
+
+describe Player do
+  subject(:player) { described_class.new('Austin') }
+
+  describe '#to_s' do
+    it 'displays the player fields in a string' do
+      player.goes_first = true
+      output = 'name: Austin, goes_first: true, turns_taken: 0, is_winner: false'
+      expect(player.to_s).to eq(output)
+    end
+  end
+end
