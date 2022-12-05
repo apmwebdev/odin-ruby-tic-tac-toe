812269085d47b2f5861c9c42a57294445ff87d49
Austin Miller <austinmillerwebdev@gmail.com>
2022-12-05 10:09:59 -0800

    Add test for #register_player

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,21 @@
 # frozen_string_literal: true
 
+require './lib/game'
+require './lib/board'
+require './lib/player'
+
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe '#register_player' do
+    before do
+      player1 = 'Austin'
+      player2 = 'Bob'
+      allow(game).to receive(:gets).and_return(player1, player2)
+    end
+
+    it 'increases length of @players by 1' do
+      expect { game.send(:register_player) }.to change { game.instance_variable_get(:@players).length }.by(1)
+    end
+  end
+end
